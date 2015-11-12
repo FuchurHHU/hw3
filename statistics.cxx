39cdd0cffbcab150e2f376f1e66ee9a23f51cb52
Live session user <mint@mint.(none)>
2015-11-12 14:54:07 +0000

    HA3 Ausgabe ist immer die selbe

--- statistics.cxx
+++ statistics.cxx
@@ -7,12 +7,56 @@ using namespace std;
 // plenty of lines here
 //--------------------------------------------------
 
+void Zufallszahlen(const int N, double* p){
+  for (int i=0;i<N;i++){ 
+   
+    p[i] = rand(); //int rand () gab bei mir eine Fehlermeldung
+   
+  }
+   
+   
+}
+
+double Mean(double* p,const int N){
+  
+ 
+  double sum = 0;
+  
+  for (int i = 0; i<N ; i++){
+   sum = p[i] + sum;
+    
+  }
+    
+  double mean = sum/N;
+  return mean;
+
+  
+}
+
+double Var(double* p, const int N, double m){
+
+  double sum = 0;
+  for (int i=0;i<N;i++){
+    sum = (p[i]-m)*(p[i]-m);
+    }
+    
+  double var = sum/N;
+  return var;
+}
+
+
+
+
+
 int main(){
    const int N = 100;
-   double p[N];
+   double p[N]; //array with N values  
    double mean, var;
 
-   // Some lines here....
+   Zufallszahlen(N,p);
+   mean = Mean(p,N);
+   var = Var(p,N,mean);
+
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
